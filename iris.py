23e553caae4249fdae34f047bb5ddcff71024bd2
Cer0n <comcesar1@gmail.com>
2024-08-28 12:56:22 -0600

    Solucion de bug aun

--- iris.py
+++ iris.py
@@ -1,6 +1,6 @@
 #nuevo iris version 3.0 al 2024
 from Procesos import Procesos
-version = "3.0.3"
+version = "3.0.4"
 p = Procesos()
 print(f"Iris version: {version}")
 p.MX = int(input("ingrese la cantidad de caracteres: "))
@@ -9,29 +9,29 @@ codigo = input("ingrese su codigo EJX: ")
 plataforma = input("ingrese su plataforma: ")
 
 contra = p.Viegener(clave,plataforma)
-print(contra)
+# print(contra)
 for i in codigo:
     if i == "1":
         contra  = p.moduloR(contra)
-        print(f"1: {contra}")
+        # print(f"1: {contra}")
     elif i == "2":
         contra  = p.moduloC(contra)  
-        print(f"2: {contra}") 
+        # print(f"2: {contra}") 
     elif i == "3":
         contra  = p.Fibonazi(contra) 
-        print(f"3: {contra}")
+        # print(f"3: {contra}")
     elif i == "4":
         contra  = p.baraja(contra)
-        print(f"4: {contra}")
+        # print(f"4: {contra}")
     elif i == "5":
         contra  = p.invertir(contra)
-        print(f"5: {contra}") 
+        # print(f"5: {contra}") 
     elif i == "6":
         contra  = p.hop(contra)
-        print(f"6: {contra}")
+        # print(f"6: {contra}")
     elif i == "7":
         contra  = p.calidad(contra)
-        print(f"7: {contra}")
+        # print(f"7: {contra}")
     else:
         print("no valido")
 
